b3334660417e119062ff621720aa46d9869821f1
Leandro Lopes <leandro_lopez12@yahoo.com.br>
2018-10-25 02:20:18 -0300

    save alter http request

--- src/app/restaurantes/restaurantes.service.ts
+++ src/app/restaurantes/restaurantes.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
 @Injectable()
 export class ResturantesService {
   rests: Restaurant[] = [
